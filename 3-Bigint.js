cd92b49f4c0126b02c028eed79008a011c982dcd
mujahid-khan111 <123661594+Mujahid-khan111@users.noreply.github.com>
2024-11-08 17:22:34 +0530

    Add files via upload

---
+++ 3-Bigint.js
@@ -0,0 +1,12 @@
+//  JavaScript mein, "bigint" ek data type hai jo bahut bade integers ko represent karta hai.
+//  Ye integers normal "number" data type se bade hote hain, 
+//  jinmein aapko precision ki zarurat hoti hai.
+//  BigInt ka use kisi bhi integer value ke end mein "n" suffix ke sath kiya jata hai.
+
+
+
+const largeNumber = 1234567890123456789012345678901234567890n;
+console.log(typeof largeNumber);
+
+var a = 42n;
+console.log(typeof a);
